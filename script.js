8deb5fb0af2e103f132a6969186b4aaddcb8d397
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-06 18:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-//your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const infiList = document.getElementById("infi-list");
+  let currentItems = 10;
 
+  function addList(start, end) {
+    for (let index = start; index < end; index++) {
+      const element = document.createElement("li");
+      element.textContent = `Item ${index + 1}`;
+      infiList.appendChild(element);
+    }
+  }
+
+  addList(0, currentItems);
+  function isScrollBottom() {
+    return (
+      infiList.scrollHeight - infiList.clientHeight <= infiList.scrollTop + 1
+    );
+  }
+
+  infiList.addEventListener("scroll", function () {
+    if (isScrollBottom()) {
+      currentItems += 2; 
+      addList(currentItems - 2, currentItems);
+    }
+  });
+});
